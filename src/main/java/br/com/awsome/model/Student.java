ed403dd05f21952e4f4d0102242a8752c7c8f669
Joaquim Francisco Vieira Junior <joaquimjuniorsf@gmail.com>
2020-03-24 23:37:49 -0300

    primeiro commit

---
+++ src/main/java/br/com/awsome/model/Student.java
@@ -0,0 +1,12 @@
+package br.com.awsome.model;
+
+public class Student {
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public Student() {
+    }
+}
---
+++ src/main/java/br/com/awsome/start/AplicationStart.java
@@ -0,0 +1,4 @@
+package br.com.awsome.start;
+
+public class AplicationStart {
+}
